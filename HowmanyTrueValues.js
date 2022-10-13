98bb409388bfdb8ef07c42d49572b701bcc9992e
ayushpardeshi12 <106296493+ayushpardeshi12@users.noreply.github.com>
2022-10-13 13:52:50 +0530

    Tocheck True values in the function or array

---
+++ HowmanyTrueValues.js
@@ -0,0 +1,11 @@
+function TRUE(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(TRUE([true, false, true, false, true, false]));
